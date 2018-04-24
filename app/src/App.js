30ee01178dda8a9cebb6b6bd374b3ad5fc74e55d
CHBaker <charlesprobaker@mail.com>
2018-04-24 17:33:19 -0600

    initial commit

---
+++ app/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className='row'>
+                    <div className='col'>
+                        stuff 1
+            </div>
+                    <div className='col'>
+                        stuff 2
+            </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        stuff 1
+            </div>
+                    <div className='col'>
+                        stuff 2
+            </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
